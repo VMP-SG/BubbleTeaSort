86f4f6d9c9d19ef39681a24413ae2892eced35b2
kevinchangjk <kevinchangjk@gmail.com>
2023-01-14 20:05:01 +0800

    Add state for new post screen

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -1,6 +1,15 @@
 import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
 
 const NewPostScreen = ({ navigation }) => {
+  const [store, setStore] = useState("");
+  const [flavours, setFlavours] = useState([]);
+  const [photoUri, setPhotoUri] = useState("");
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [price, setPrice] = useState("");
+  const [comments, setComments] = useState("");
+
   return (
     <View>
       <Text>This is the New Post Screen</Text>
